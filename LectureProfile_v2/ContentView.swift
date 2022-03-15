4cf052d9fae11b0667577f48d56296c8d9a7ec81
Owen Hildreth <ohildreth@mines.edu>
2022-03-15 13:47:23 -0600

    Initial Commit

---
+++ LectureProfile_v2/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  LectureProfile_v2
+//
+//  Created by Owen Hildreth on 3/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var graphController = GraphController()
+    
+    var body: some View {
+        VStack{
+            HStack {
+                Slider(value: $graphController.adjustableValue, in: 0.0...10.0) {
+                    Text(String(format: "%.2f", graphController.adjustableValue))
+                }
+                Text(String(format: "%.2f", graphController.computedValue))
+            }
+            
+            Button("Increment"){graphController.updateData()}
+            GraphViewRepresentable(graphController: graphController)
+                .frame(minWidth: 200, maxWidth: .infinity, minHeight: 200, maxHeight: .infinity)
+        }
+        .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LectureProfile_v2/LectureProfile_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  LectureProfile_v2App.swift
+//  LectureProfile_v2
+//
+//  Created by Owen Hildreth on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LectureProfile_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
